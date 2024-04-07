817edf8a5f6abe4ac2abe421b358e4f4b310ca2a
pablotroncoso <pablotroncoso@gmail.com>
2024-04-06 21:41:51 -0300

    Actualización Index y nombre de imagen :)

--- index.js
+++ index.js
@@ -1,36 +1,51 @@
 // Importar Express
 const express = require('express');
 
-//Instanciar Express
+// Instanciar Express
 const app = express();
 
-//Levantar el Servidor
+// Levantar el Servidor
 app.listen(3000, () => {
   console.log('Servidor escuchando por el puerto 3000');
 });
 
-//Middleware de uso General
+// Middleware de uso General
 app.use(express.static("assets"));
 
-//Arreglo 4 Nombres
+// Arreglo 4 Nombres
 const nombres= ["Pablo", "Francisco","Juan", "Maria"];
 
-//Creación Rutas
+// Creación Rutas
 app.get("/abracadabra/usuarios", (req, res) => {
   res.send({ nombres })
 })
   
-//Creación Middleware de la Segunda Ruta
+// Creación Middleware de la Segunda Ruta
 app.use("/abracadabra/juego/:usuario", (req, res, next) => {
   nombres.includes(req.params.usuario) ? next() : res.redirect("/who.jpeg") 
 });
 
+// Juego
 app.get("/abracadabra/juego/:usuario", (req, res) => {
   res.sendFile(__dirname + '/index.html') 
 });
 
+// Conejo
+app.get("/abracadabra/conejo/:n", (req, res) => {
+  const randomNumber = Math.floor(Math.random() * 4) + 1; 
+  const n = parseInt(req.params.n); 
 
+  if (n == randomNumber) {
+        res.sendFile(__dirname + '/assets/conejo.jpg');
+  } else {
+       res.sendFile(__dirname + '/assets/voldemort.jpg');
+  }
+});
 
+// Pagina no existe
+app.use((req, res) => {
+  res.status(404).send("Oh oh! Esta página no existe");
+});
 
 
 
